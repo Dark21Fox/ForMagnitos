4d5a752eb5cc0079719224667e6e4e6eb4a91db6
John <dark21fox@gmail.com>
2026-03-13 13:51:56 +0300

    fix

--- powershell/nagruzochka.ps1
+++ powershell/nagruzochka.ps1
@@ -1,25 +1,38 @@
 # RAM
 $os = Get-CimInstance Win32_OperatingSystem
-$totalRAM = [math]::Round($os.TotalVisibleMemorySize / 1MB, 2)
-$usedRAM  = [math]::Round(($os.TotalVisibleMemorySize - $os.FreePhysicalMemory) / 1MB, 2)
+$totalRAM = $os.TotalVisibleMemorySize / 1MB
+$freeRAM  = $os.FreePhysicalMemory / 1MB
 
-# SWAP (PageFile)
-$pageFile = Get-CimInstance Win32_PageFileUsage
+# Кэш памяти (Standby list)
+$memInfo  = Get-CimInstance Win32_PerfFormattedData_PerfOS_Memory
+$cachedRAM = $memInfo.StandbyCacheNormalPriorityBytes / 1GB + 
+             $memInfo.StandbyCacheCoreBytes / 1GB +
+             $memInfo.StandbyCacheReserveBytes / 1GB
+
+$usedRAM      = [math]::Round($totalRAM - $freeRAM, 2)
+$ramUtil      = [math]::Round(($totalRAM - ($freeRAM + $cachedRAM)) / $totalRAM * 100, 2)
+$totalRAM_GB  = [math]::Round($totalRAM, 2)
+
+# SWAP
+$pageFile  = Get-CimInstance Win32_PageFileUsage
 $totalSwap = [math]::Round($pageFile.AllocatedBaseSize / 1KB, 2)
 $usedSwap  = [math]::Round($pageFile.CurrentUsage / 1KB, 2)
+$swapUtil  = if ($totalSwap -gt 0) { [math]::Round($usedSwap / $totalSwap * 100, 2) } else { 0 }
 
 # CPU
-$cpu = (Get-CimInstance Win32_Processor | Measure-Object -Property LoadPercentage -Average).Average
+$cpuUtil = (Get-CimInstance Win32_Processor | Measure-Object -Property LoadPercentage -Average).Average
 
 # Диски
 $disks = Get-CimInstance Win32_LogicalDisk -Filter "DriveType=3" | ForEach-Object {
-    $total = [math]::Round($_.Size / 1GB, 2)
-    $used  = [math]::Round(($_.Size - $_.FreeSpace) / 1GB, 2)
-    "$($_.DeviceID) Total:${total}GB Used:${used}GB"
+    $total    = [math]::Round($_.Size / 1GB, 2)
+    $free     = [math]::Round($_.FreeSpace / 1GB, 2)
+    $used     = [math]::Round($total - $free, 2)
+    $diskUtil = [math]::Round(($total - $free) / $total * 100, 2)
+    "$($_.DeviceID) Total:${total}GB Used:${used}GB Free:${free}GB Util:${diskUtil}%"
 }
 $disksStr = $disks -join "; "
 
 # Итоговая строка
-$result = "RAM Total:${totalRAM}GB Used:${usedRAM}GB|SWAP Total:${totalSwap}GB Used:${usedSwap}GB|CPU Load:${cpu}%|DISKS: $disksStr"
+$result = "RAM Total:${totalRAM_GB}GB Used:${usedRAM}GB Util:${ramUtil}%|SWAP Total:${totalSwap}GB Used:${usedSwap}GB Util:${swapUtil}%|CPU Util:${cpuUtil}%|DISKS: $disksStr"
 
 Write-Output $result
